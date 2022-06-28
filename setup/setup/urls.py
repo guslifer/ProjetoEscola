700767a0c898e3d462dafd2edc20bd3036793eb1
guslifer <gustavolinofernandes@gmail.com>
2022-06-27 22:20:01 -0300

    documentation

--- setup/setup/urls.py
+++ setup/setup/urls.py
@@ -17,6 +17,24 @@ from django.contrib import admin
 from django.urls import path, include
 from escola.views import AlunosViewSet, CursosViewSet, MatriculasViewSet, ListaMatriculasAluno,ListaAlunosMatriculados
 from rest_framework import routers
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Alura School",
+      default_version='v1',
+      description="Provedor Local de relacionamentos de cursos e alunos em uma escola, projeto base para aplicações django rest framework",
+      terms_of_service="#",
+      contact=openapi.Contact(email="c3po@alura.com.br"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
+
 
 
 router = routers.DefaultRouter()
@@ -26,6 +44,7 @@ router.register('matriculas', MatriculasViewSet, basename="Matriculas")
 
 
 urlpatterns = [
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('api-auth/', include('rest_framework.urls')),
     path('admin/', admin.site.urls),
     path("", include(router.urls)),   
